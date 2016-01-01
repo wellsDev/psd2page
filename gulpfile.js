304ee2245640ce9ca50141b7b90e8d22feb02b00
wellsdev <charlie@childofaking.com>
2016-01-01 15:26:27 -0600

    Moved normalize to a SASS file and added BrowserSync

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,20 @@
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+var reload      = browserSync.reload;
 
-gulp.task('default', ['sass', 'sass:watch']);
+gulp.task('default', ['sass', 'sass:watch', 'browser-sync']);
+
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+    gulp.watch("*.html").on("change", reload);
+    gulp.watch("./assets/sass/*.sass").on("change", reload);
+});
 
 gulp.task('sass', function () {
   gulp.src(['./assets/sass/*.sass'])
